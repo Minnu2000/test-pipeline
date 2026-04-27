20bd5a35bbcec324de2bb695826e0e7b0e9af00e
Minnu Thampi <minnu.thampi@trenser.com>
2026-04-27 11:00:04 +0000

    minor chnage in main

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    // Print Hello World to the console
+    // Print Hello World to the console.........
     printf("Hello, World without release tag...!\n");
     return 0;
 }
